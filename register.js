c22af20573f8734bf7defe03864ba4620f3cfa45
WuyWuy <nguyenhuyv35@gmail.com>
2025-02-21 20:34:54 +0700

    Register

---
+++ register.js
@@ -0,0 +1,8 @@
+let First = document.getElementById('form1').fname.value;
+let Last  = document.getElementById('form1').lname.value;
+let Color = document.getElementById('form1');
+
+Color.style.backgroundColor = 'red';
+
+console.log(First);
+console.log(Last);
