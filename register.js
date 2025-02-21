c8ce179f5fbf4504fb1617d04bdb299404dfa0b2
WuyWuy <nguyenhuyv35@gmail.com>
2025-02-21 20:36:45 +0700

    MindXCinema

--- register.js
+++ register.js
@@ -1,8 +1,27 @@
-let First = document.getElementById('form1').fname.value;
-let Last  = document.getElementById('form1').lname.value;
-let Color = document.getElementById('form1');
+document.getElementById("Submit").addEventListener("click", function(){
 
-Color.style.backgroundColor = 'red';
+    let name = document.getElementById("Username").value;
 
-console.log(First);
-console.log(Last);
+    let email = document.getElementById("Email").value;
+
+    let pass = document.getElementById("Password").value;
+
+    if (name == "" || email == "" || pass == ""){
+
+        alert("Vui lòng nhập đầy đủ thông tin!");
+
+    } else {
+
+        let RegisterSite = document.getElementById("container");
+        let Menu = document.getElementById("menu");
+
+        RegisterSite.style.display = "none";
+        Menu.style.display = "flex";
+
+        alert("Đăng nhập thành công!");
+
+        document.body.style.backgroundColor = "gray"
+
+    }
+
+});
